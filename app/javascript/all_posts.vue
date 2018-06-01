79e7a639a9bda98f2d17bad2c895a26cf0b0eca8
Anna Lewis <anna.noel.lewis@gmail.com>
2018-06-01 11:59:21 -0600

    Can render list of posts

---
+++ app/javascript/all_posts.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="all-posts">
+    <div v-for="post in posts">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "all-posts",
+  data: function () {
+    return {
+      posts: [
+        { title: 'Learn JavaScript', content: 'Why???' },
+        { title: 'Learn Vue', content: 'Yes, do this' },
+        { title: 'Build something awesome', content: 'Also do this' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,11 @@
-import Vue from 'vue'
-import App from './components/app.vue'
+import Vue from 'vue/dist/vue.js'
+import App from '../app.vue'
+import AllPosts from '../all_posts.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
 
 document.addEventListener('DOMContentLoaded', () => {
   document.body.appendChild(document.createElement('app'))
@@ -10,4 +16,15 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   console.log(app)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.appendChild(document.createElement('all-posts'))
+  const allPosts = new Vue({
+    el: 'all-posts',
+    template: '<AllPosts/>',
+    components: { AllPosts }
+  })
+
+  console.log(allPosts)
 })
