f34d9404b32ee40c66ae02e0e00539707dbafaa1
Anna Lewis <anna.noel.lewis@gmail.com>
2018-06-01 10:44:30 -0600

    Add crud for all posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,8 +1,32 @@
 class Api::V1::PostsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    render json: Post.all
+  end
+
+  def show
+    render json: Post.find(params[:id])
+  rescue
+    render status: 404
+  end
+
   def create
     render json: Post.create(post_params)
+  rescue
+    render status: 400
+  end
+
+  def update
+    render json: Post.update(param[:id], post_params)
+  rescue
+    render status: 400
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+  rescue
+    render status: 404
   end
 
   private
@@ -12,5 +36,4 @@ class Api::V1::PostsController < ApplicationController
           .permit(:title, :content)
           .merge({user_id: current_user.id})
   end
-
 end
